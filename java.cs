0bc94517b969ed178df868b4b5a1e8f30404ab3b
sofyan95 <fyansofyan02@gmail.com>
2024-05-31 13:21:53 +0800

    Delete java.cs

--- java.cs
+++
@@ -1,8 +0,0 @@
-function calculate() {
-    var loanAmount = parseFloat(document.getElementById('loan-amount').value);
-    var loanTerm = parseFloat(document.getElementById('loan-term').value);
-    
-    var monthlyPayment = (loanAmount / loanTerm).toFixed(2);
-    
-    document.getElementById('result').innerHTML = 'Pembayaran Bulanan: Rp ' + monthlyPayment;
-}
